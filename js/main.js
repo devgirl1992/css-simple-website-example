f02307b4019a7fcd75e96031b97b7d0e642b5711
Azadeh Galidari <azadeh.galidari.1992@gmail.com>
2020-05-19 18:08:37 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Initialize and add the map
+function initMap() {
+  // your location
+  const loc = { lat: 52.520008, lng: 13.404954 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: mpa });
+}
+
+// Sticky menu background
+window.addEventListener("scroll", function() {
+  if(window.scrollY > 150) {
+   document.querySelector("#navbar").style.opacity = 0.5;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top - 100
+    },
+    800
+    );
+  }
+});
